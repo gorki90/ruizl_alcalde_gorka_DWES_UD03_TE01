b24198cac7ec7092d6d5a8c89a7a93d99030d5be
gorka <gorruiz@birt.eus>
2023-12-10 16:24:00 +0200

    medio

---
+++ API/v1/app/controllers/post.php
@@ -0,0 +1,13 @@
+<?php
+class Post{
+function __construct(){
+    echo "Hola";
+}
+
+function new(){
+    echo "Hola desde new";
+}
+
+}
+
+?>
--- API/v1/public/index.php
+++ API/v1/public/index.php
@@ -1,3 +1,63 @@
+<?php
+require "../core/router.php";
+require "../app/controllers/post.php";
+
+$router=new Router();
+
+$router->add('/public', array(
+    'controller'=>'Home',
+    'action'=>'index'
+));
+
+$router->add('/public/post/new', array(
+    'controller'=>'Post',
+    'action'=>'new'
+));
+
+$url= $_SERVER["QUERY_STRING"];
+echo "URL =".$url. "<br>";
+
+$urlParams=explode('/',$url);
+
+$urlArray=array(
+'HTTP'=>$_SERVER['REQUEST_METHOD'],
+'path'=>$url,
+'controller'=>'',
+'action'=>'',
+'params'=>''
+);
+
+if(!empty($urlParams[2])){
+$urlArray['controller']=ucwords($urlParams[2]);
+if(!empty($urlParams[3])){
+$urlArray['action']=$urlParams[3];
+if(!empty($urlParams[4])){
+$urlArray['params']=$urlParams[4];
+}
+}else{
+    $urlArray['action']='index';
+}
+}else{
+    $urlArray['controller']='Home';
+    $urlArray['action']='index';
+}
+
+echo '<pre>';
+print_r($urlArray).'<br>';
+echo '</pre>';
+
+if($router->match($urlArray)){
+$controller=$router->getParams()['controller'];
+$action=$router->getParams()['action'];
+$controller=new $controller();
+$controller->$action();
+
+}else{
+   echo "Route not found ".$url; 
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
